78fe1ea809fe2993f227d69a8c2d76eaeb2732d3
Jonathan Pohahau <j.pohahau@outlook.com>
2023-02-07 17:16:42 -0800

    added homepage hb, User model, and blog model

---
+++ models/Blog.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    blog_post: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    exhibition_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blog',
+  }
+);
+
+module.exports = Painting;
