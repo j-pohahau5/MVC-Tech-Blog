e1be36695e278b95b9161df2fe7869601724103c
Jonathan Pohahau <j.pohahau@outlook.com>
2023-02-07 19:54:29 -0800

    added login handlebars & js signup handlebars & js

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -1,22 +1,19 @@
-const router = require('express').Router();
-const { Comment, Blog, User } = require('../models');
+const router = require("express").Router();
+const { Comment, Blog, User } = require("../models");
 
-// GET all galleries for homepage
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const dbBlogData = await Blog.findAll({
       include: [
         {
           model: User,
-          attributes: ['username'],
+          attributes: ["username"],
         },
       ],
     });
 
-    const blogs = dbBlogData.map((blog) =>
-      blog.get({ plain: true })
-    );
-    res.render('homepage', {
+    const blogs = dbBlogData.map((blog) => blog.get({ plain: true }));
+    res.render("homepage", {
       blogs,
       loggedIn: req.session.loggedIn,
     });
@@ -26,118 +23,92 @@ router.get('/', async (req, res) => {
   }
 });
 
-// GET one gallery
-router.get('/blog/:id', async (req, res) => {
+router.get("/blog/:id", async (req, res) => {
   try {
     const dbBlogData = await Blog.findByPk(req.params.id, {
       include: [
         {
           model: Comment,
-          attributes: [
-            'id',
-            'text',
-            'date',
-            'user_id',
-          ],
+          attributes: ["id", "text", "date", "user_id"],
           include: [
             {
               model: User,
-              attributes: [
-                'id',
-                'username',
-              ],
-            }
+              attributes: ["id", "username"],
+            },
           ],
         },
         {
-            model: User,
-            attributes: [
-              'id',
-              'username',
-            ],
+          model: User,
+          attributes: ["id", "username"],
         },
       ],
     });
 
     const blog = dbBlogData.get({ plain: true });
-    res.render('blog', { blog, loggedIn: req.session.loggedIn });
+    res.render("blog", { blog, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
-// GET one painting
-router.get('/painting/:id', async (req, res) => {
+router.post("/blog", async (req, res) => {
   try {
-    const dbPaintingData = await Painting.findByPk(req.params.id);
+    const dbBlogData = await Blog.create({
+      title: req.body.title,
+      content: req.body.content,
+    });
 
-    const painting = dbPaintingData.get({ plain: true });
-    res.render('painting', { painting, loggedIn: req.session.loggedIn });
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbBlogData);
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
+  }
+});
+
+router.post("/blog/:id/comment", async (req, res) => {
+  try {
+    const dbBlogData = await Blog.findOne({
+      where: {
+        id: req.body.id,
+      },
+    });
+    const dbCommentData = await Comment.create({
+      text: req.body.text,
+    });
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: dbCommentData, message: "You added a comment!" });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
   }
 });
 
 // Login route
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-  res.render('login');
-});
-
-router.post('/blog', async (req, res) => {
-    try {
-      const dbBlogData = await Blog.create({
-        title: req.body.title,
-        content: req.body.content,
-      });
-  
-      req.session.save(() => {
-        req.session.loggedIn = true;
-  
-        res.status(200).json(dbBlogData);
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
-  
-  router.post('/blog/:id/comment', async (req, res) => {
-    try {
-      const dbBlogData = await Blog.findOne({
-        where: {
-          id: req.body.id,
-        },
-      });
-      const dbCommentData = await Comment.create({
-        text: req.body.text,
-      }); 
-      req.session.save(() => {
-        req.session.loggedIn = true;
-  
-        res
-          .status(200)
-          .json({ user: dbCommentData, message: 'You added a comment!' });
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
-  
-  router.post('/logout', (req, res) => {
+router.get("/login", (req, res) => {
     if (req.session.loggedIn) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
+      res.redirect("/");
+      return;
     }
+    res.render("login");
   });
-
 module.exports = router;
